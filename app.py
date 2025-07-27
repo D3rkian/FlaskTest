1d7eeee616cc4d9a82eadb83db1ab8f6e97a192d
D3rkian <derkian.f@escolar.ifrn.edu.br>
2025-07-27 01:09:30 +0000

    primeiro

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+caminho_db = os.path.join(os.path.abspath(os.path.dirname(__file__)), os.getenv('DB_PATH'))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{caminho_db}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate.init_app(app, db)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
